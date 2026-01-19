a07a0be81fb7c32630d50624832ed64cde80378b
Elena Montes <elenam_89@hotmail.com>
2026-01-19 18:17:44 +0100

    commit inicial

---
+++ src/main/java/com/universae/quiz/CarnavalQuizApplication.java
@@ -0,0 +1,13 @@
+package com.universae.quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CarnavalQuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CarnavalQuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/universae/quiz/CarnavalQuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.universae.quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CarnavalQuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
